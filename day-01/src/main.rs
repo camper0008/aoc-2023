a2cffa0021896d3f179694d5cad65e7fc1106393
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-12-02 23:02:11 +0100

    initial commit

---
+++ day-01/src/main.rs
@@ -0,0 +1,163 @@
+fn first() {
+    let input = include_str!("input.txt");
+    let output: usize = input.lines().map(|v| -> usize {
+        let first = v.chars().find(char::is_ascii_digit).unwrap();
+        let last = v.chars().rev().find(char::is_ascii_digit).unwrap();
+        (first.to_string() + &last.to_string()).parse::<usize>().unwrap()
+    })
+        .sum();
+    println!("first: {output}");
+}
+
+fn second() {
+    let input = include_str!("input.txt");
+    let output: usize = input.lines().map(|chars| -> usize {
+        let mut first: Option<String> = None;
+        let mut last: Option<String> = None;
+        let mut idx = 0;
+        loop {
+            match &chars.get(idx..=idx) {
+                Some(v) if v.chars().last().unwrap().is_ascii_digit() => {
+                    if first.is_none() {
+                        first = Some((*v).to_string());
+                    } else {
+                        last = Some((*v).to_string());
+                    };
+                    idx += 1;
+                }
+                Some("o") /* one */ => {
+                    let m = "one";
+                    let v = Some("1".to_string());
+                    if chars.get(idx..idx+m.len()) == Some(m) {
+                        if first.is_none() {
+                            first = v;
+                        } else {
+                            last = v;
+                        };
+                        idx += m.len() - 1;
+                    } else {
+                        idx += 1;
+                    }
+                }
+                Some("t") /* two three */ => {
+                    let m = "two";
+                    let v = Some("2".to_string());
+                    if chars.get(idx..idx+m.len()) == Some(m) {
+                        if first.is_none() {
+                            first = v;
+                        } else {
+                            last = v;
+                        };
+                        idx += m.len() - 1;
+                        continue;
+                    }
+                    let m = "three";
+                    let v = Some("3".to_string());
+                    if chars.get(idx..idx+m.len()) == Some(m) {
+                        if first.is_none() {
+                            first = v;
+                        } else {
+                            last = v;
+                        };
+                        idx += m.len() - 1;
+                        continue;
+                    }
+                    idx += 1;
+                }
+                Some("f") /* four five */ => {
+                    let m = "four";
+                    let v = Some("4".to_string());
+                    if chars.get(idx..idx+m.len()) == Some(m) {
+                        if first.is_none() {
+                            first = v;
+                        } else {
+                            last = v;
+                        };
+                        idx += m.len() - 1;
+                        continue;
+                    }
+                    let m = "five";
+                    let v = Some("5".to_string());
+                    if chars.get(idx..idx+m.len()) == Some(m) {
+                        if first.is_none() {
+                            first = v;
+                        } else {
+                            last = v;
+                        };
+                        idx += m.len() - 1;
+                        continue;
+                    }
+                    idx += 1;
+
+                }
+                Some("s") /* six seven */ => {
+                    let m = "six";
+                    let v = Some("6".to_string());
+                    if chars.get(idx..idx+m.len()) == Some(m) {
+                        if first.is_none() {
+                            first = v;
+                        } else {
+                            last = v;
+                        };
+                        idx += m.len() - 1;
+                        continue;
+                    }
+                    let m = "seven";
+                    let v = Some("7".to_string());
+                    if chars.get(idx..idx+m.len()) == Some(m) {
+                        if first.is_none() {
+                            first = v;
+                        } else {
+                            last = v;
+                        };
+                        idx += m.len() - 1;
+                        continue;
+                    }
+                    idx += 1;
+                }
+                Some("e") /* eight */ => {
+                    let m = "eight";
+                    let v = Some("8".to_string());
+                    if chars.get(idx..idx+m.len()) == Some(m) {
+                        if first.is_none() {
+                            first = v;
+                        } else {
+                            last = v;
+                        };
+                        idx += m.len() - 1;
+                    } else {
+                        idx += 1;
+                    }
+
+                }
+                Some("n") /* nine */ => {
+                    let m = "nine";
+                    let v = Some("9".to_string());
+                    if chars.get(idx..idx+m.len()) == Some(m) {
+                        if first.is_none() {
+                            first = v;
+                        } else {
+                            last = v;
+                        };
+                        idx += m.len() - 1;
+                    } else {
+                        idx += 1;
+                    }
+                }
+                Some(_) => idx += 1,
+                None => break,
+            };
+        };
+        let first = first.unwrap();
+        let last = last.unwrap_or(first.clone());
+        (first + &last).parse().unwrap()
+    })
+        .sum();
+    println!("second: {output}");
+
+}
+
+fn main() {
+    first();
+    second();
+}
---
+++ day-02/src/main.rs
@@ -0,0 +1,109 @@
+fn first() {
+    let input = include_str!("input.txt");
+    let output = input.lines().filter_map(|v| {
+        let sets: Vec<_> = v.split(":").collect();
+        let game_id = sets[0].split_whitespace().skip(1).collect::<Vec<_>>()[0];
+        let game_id: usize = game_id.parse().unwrap();
+        let sets = sets[1];
+        let sets = sets.split(";");
+        for set in sets {
+            let items = set.split(",").map(|v| v.trim());
+            for item in items {
+                let count_and_color: Vec<_> = item.split_whitespace().filter(|v| !v.is_empty()).collect();
+                let count = count_and_color[0];
+                let count: usize = count.parse().unwrap();
+                let color = count_and_color[1];
+                match color {
+                    "red" => {
+                        if count > 12 {
+                            return None;
+                        }
+                    }
+                    "green" => {
+                        if count > 13 {
+                            return None;
+                        }
+                    }
+                    "blue" => {
+                        if count > 14 {
+                            return None;
+                        }
+                    }
+                    color => unreachable!("unexpected color {color}"),
+                }
+            };
+        };
+        Some(game_id)
+    }).sum::<usize>();
+    println!("{output}");
+}
+
+#[derive(Default, Clone)]
+struct Count {
+    red: usize,
+    green: usize,
+    blue: usize,
+}
+
+impl Count {
+    fn set_red(&mut self, value: usize) {
+        if value > self.red {
+            self.red = value;
+        }
+    }
+    fn set_green(&mut self, value: usize) {
+        if value > self.green {
+            self.green = value;
+        }
+    }
+    fn set_blue(&mut self, value: usize) {
+        if value > self.blue {
+            self.blue = value;
+        }
+    }
+    fn power(&self) -> usize {
+        self.red * self.green * self.blue
+    }
+}
+
+fn second() {
+    let input = include_str!("input.txt");
+    let mut games = vec![Count::default(); input.lines().count()];
+    let output = input.lines().map(|v| {
+        let sets: Vec<_> = v.split(":").collect();
+        let game_id = sets[0].split_whitespace().skip(1).collect::<Vec<_>>()[0];
+        let game_id: usize = game_id.parse().unwrap();
+        let game_id = game_id - 1;
+        let sets = sets[1];
+        let sets = sets.split(";");
+        for set in sets {
+            let items = set.split(",").map(|v| v.trim());
+            for item in items {
+                let count_and_color: Vec<_> = item.split_whitespace().filter(|v| !v.is_empty()).collect();
+                let count = count_and_color[0];
+                let count: usize = count.parse().unwrap();
+                let color = count_and_color[1];
+                match color {
+                    "red" => {
+                        games[game_id].set_red(count);           
+                    }
+                    "green" => {
+                        games[game_id].set_green(count);           
+                    }
+                    "blue" => {
+                        games[game_id].set_blue(count);           
+                    }
+                    color => unreachable!("unexpected color {color}"),
+                }
+            };
+        };
+        games[game_id].power()
+    }).sum::<usize>();
+    println!("{output}");
+
+}
+
+fn main() {
+    first();
+    second();
+}
